36d84fa7e37fb138a335886b85af7023baceb4d7
iSeeGor <iseegor87@gmail.com>
2019-09-06 21:40:16 +0300

    Подготовка файлов

---
+++ home.php
@@ -0,0 +1,13 @@
+<div class="site">
+    <header class="site-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Turkey State</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, veniam?</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, atque!</p>
+                </div>
+            </div>
+        </div>        
+    </header>
+</div>
