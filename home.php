f190a672e19210f1775ea9fb20c010d08fff3eff
iSeeGor <iseegor87@gmail.com>
2019-09-10 19:44:24 +0300

    w

--- home.php
+++ home.php
@@ -4,6 +4,7 @@
             <div class="row">
                 <div class="col-12">
                     <h1>Turkey State</h1>
+                    <h3>Turkey State H3</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, veniam?</p>
                 </div>
             </div>
