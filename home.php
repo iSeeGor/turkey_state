6afeb2a113b27590d79c611222c72fda093c8f45
iSeeGor <iseegor87@gmail.com>
2019-09-15 12:08:30 +0300

    Add Footer

--- home.php
+++ home.php
@@ -1,13 +1,3 @@
-<div class="site">
-    <header class="site-header">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h1>Turkey State</h1>
-                    <h3>Turkey State H3</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, veniam?</p>
-                </div>
-            </div>
-        </div>        
-    </header>
-</div>
+<section class="section site-slider">
+    <img src="/assets/images/slider.jpg" alt="">
+</section>
