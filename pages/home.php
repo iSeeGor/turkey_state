3c4a567ab1940e4cf5fae7f6a9b4d3521cffc7f0
iSeeGor <iseegor@gmail.com>
2019-11-18 10:02:33 +0200

    18.11.2019

--- pages/home.php
+++ pages/home.php
@@ -1197,29 +1197,98 @@
     <div class="container">
         <div class="testimonialSlider__container swiper-container">
             <div class="testimonialSlider__wrapper swiper-wrapper">
+                <!-- slide 01 -->
                 <div class="testimonialSlider__slide swiper-slide">
-                    <h3>Slide 01</h3>
-                    <p>Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев
-                     более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык
-                        публичных выступлений в домашних условиях. При создании генератора мы использовали небезизвестный
-                      универсальный код речей. Текст генерируется абзацами случайным образом от двух до десяти предложений в абзаце, 
-                      что позволяет сделать текст более привлекательным и живым для визуально-слухового восприятия.</p>
+                    <header class="testimonialSlider__header">
+                                                <figure class="testimonialSlider__figure">
+                            <div class="testimonialSlider__figure-inner">
+                                <img class="testimonialSlider__user-image" src="../assets/images/content/testimonial/testimonial-user-image.jpg" alt="Testimonial User Image">
+                            </div>
+                        </figure>
+                        <div class="testimonialSlider-meta">
+                            <div class="testimonialSlider-meta__user-name">Ирина Степановна</div>
+                            <div class="testimonialSlider-meta__address">Алания</div>
+                        </div>
+                        <div class="testimonialSlider__date">14.04.2017</div>
+                    </header>
+                    <div class="testimonialSlider__body">
+                        <div class="testimonialSlider__message">
+                            <p>Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев 
+                                более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык 
+                                публичных выступлений в домашних условиях. При создании генератора мы использовали 
+                                небезизвестный универсальный код речей. Текст генерируется абзацами случайным образом от 
+                                двух до десяти предложений в абзаце, что позволяет сделать текст более привлекательным и 
+                                живым для визуально-слухового восприятия.</p>
+                        </div>
+                    </div>
                 </div>
+                <!-- slide 02 -->
                 <div class="testimonialSlider__slide swiper-slide">
-                    <h3>Slide 02</h3>
-                    <p>Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев
-                     более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык
-                        публичных выступлений в домашних условиях. При создании генератора мы использовали небезизвестный
-                      универсальный код речей. Текст генерируется абзацами случайным образом от двух до десяти предложений в абзаце, 
-                      что позволяет сделать текст более привлекательным и живым для визуально-слухового восприятия.</p>
+                    <header class="testimonialSlider__header">
+                        <figure class="testimonialSlider__figure">
+                            <div class="testimonialSlider__figure-inner">
+                                <img class="testimonialSlider__user-image" src="../assets/images/content/testimonial/testimonial-user-image-02.jpg" alt="Testimonial User Image">
+                            </div>
+                        </figure>
+                        <div class="testimonialSlider-meta">
+                            <div class="testimonialSlider-meta__user-name">Виктор Иванов</div>
+                            <div class="testimonialSlider-meta__address">Стамбул</div>
+                        </div>
+                        <div class="testimonialSlider__date">16.04.2019</div>
+                    </header>
+                    <div class="testimonialSlider__body">
+                        <div class="testimonialSlider__message">
+                            <p>Уже по прибытию в Тбилиси мы "галопом по Европам" начали составлять план, 
+                                а что же именно нам нужно посетить. Вариантов было масса. Но посоветовавшись с нашим гостом, 
+                                у которого мы снимали жилье, решили, что нужно будет непременно заглянуть и в первую столицу Грузии - 
+                                городок Мцхета. На третий день в Тбилиси мы взяли в аренду машину и направились в два небольших 
+                                приключения. Первым из них и стала Мцхета.</p>
+                        </div>
+                    </div>
                 </div>
+                <!-- slide 03 -->
                 <div class="testimonialSlider__slide swiper-slide">
-                    <h3>Slide 03</h3>
-                    <p>Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать несколько абзацев
-                     более менее осмысленного текста рыбы на русском языке, а начинающему оратору отточить навык
-                        публичных выступлений в домашних условиях. При создании генератора мы использовали небезизвестный
-                      универсальный код речей. Текст генерируется абзацами случайным образом от двух до десяти предложений в абзаце, 
-                      что позволяет сделать текст более привлекательным и живым для визуально-слухового восприятия.</p>
+                    <header class="testimonialSlider__header">
+                        <figure class="testimonialSlider__figure">
+                            <div class="testimonialSlider__figure-inner">
+                                <img class="testimonialSlider__user-image" src="../assets/images/content/testimonial/testimonial-user-image-03.jpg" alt="Testimonial User Image">
+                            </div>
+                        </figure>
+                        <div class="testimonialSlider-meta">
+                            <div class="testimonialSlider-meta__user-name">Светлана Юрьевна</div>
+                            <div class="testimonialSlider-meta__address">Милан</div>
+                        </div>
+                        <div class="testimonialSlider__date">10.12.2018</div>
+                    </header>
+                    <div class="testimonialSlider__body">
+                        <div class="testimonialSlider__message">
+                            <p>Понравился отель сразу тем что напоминает корабль да ещё на воде(6000м2 бассейны) 
+                                очень хорошее обслуживание Питание очень вкусно и на каждом шагу можешь кушать весь день. 
+                                Красивая ухоженная территория Хотелось бы ещё одну точку поближе к морю для выдачи полотенец. 
+                                Спасибо всему обслуживающему персоналу за хорошую работу.Удачи вашему отелю так держать.Семья Лукьянченко</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- slide 04 -->
+                <div class="testimonialSlider__slide swiper-slide">
+                    <header class="testimonialSlider__header">
+                        <figure class="testimonialSlider__figure">
+                            <div class="testimonialSlider__figure-inner">
+                                <img class="testimonialSlider__user-image" src="../assets/images/content/testimonial/testimonial-user-image-04.jpg" alt="Testimonial User Image">
+                            </div>
+                        </figure>
+                        <div class="testimonialSlider-meta">
+                            <div class="testimonialSlider-meta__user-name">Светлана Юрьевна</div>
+                            <div class="testimonialSlider-meta__address">Милан</div>
+                        </div>
+                        <div class="testimonialSlider__date">10.12.2018</div>
+                    </header>
+                    <div class="testimonialSlider__body">
+                        <div class="testimonialSlider__video">
+                        <iframe width="200" height="200" src="https://www.youtube.com/embed/FT0odx2LoyQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
                 </div>
             </div>
 
@@ -1228,6 +1297,7 @@
                     <li class="testimonialSlider__thumbs-items"><h2>1</h2></li>
                     <li class="testimonialSlider__thumbs-items"><h2>2</h2></li>
                     <li class="testimonialSlider__thumbs-items"><h2>3</h2></li>
+                    <li class="testimonialSlider__thumbs-items"><h2>4</h2></li>
                 </ul>
             </div>
         </div>
